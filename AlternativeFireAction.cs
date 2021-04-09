2dcfe4107faba573fc2f1df20993b509c4d80af9
ds5678 <jrpritts7@gmail.com>
2021-04-09 14:34:28 -0400

    Add project files.

---
+++ AlternativeFireAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using ModComponentAPI;
+using System;
+
+namespace InfinityFires
+{
+    internal class AlternativeFireAction : AlternativeAction
+    {
+        public AlternativeFireAction(IntPtr intPtr) : base(intPtr) { }
+
+        public override void Execute()
+        {
+            Fire fire = this.gameObject.GetComponent<Fire>();
+            
+            if (fire) fire.m_IsPerpetual = !fire.m_IsPerpetual;
+            else
+            {
+                fire = this.gameObject.GetComponentInChildren<Fire>();
+                if (fire) fire.m_IsPerpetual = !fire.m_IsPerpetual;
+                else MelonLoader.MelonLogger.LogError("Attached object doesn't have a fire component.");
+            } 
+        }
+    }
+}
