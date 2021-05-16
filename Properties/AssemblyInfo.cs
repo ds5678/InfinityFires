0603601b712b9830fdcccd94b7025e95dc9db55e
ds5678 <jrpritts7@gmail.com>
2021-05-16 04:46:53 -0400

    Compatibility Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,20 +1,20 @@
 ﻿using MelonLoader;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("InfinityFires")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InfinityFires")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+using BuildInfo = InfinityFires.BuildInfo;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("6de50eb8-5a17-486e-97e6-106a413aaae9")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: MelonInfo(typeof(InfinityFires.Implementation), "InfinityFires", "1.0.0", "ds5678")]
+[assembly: AssemblyTitle(BuildInfo.Name)]
+[assembly: AssemblyDescription(BuildInfo.Description)]
+[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
+[assembly: AssemblyTrademark(BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion(BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+[assembly: MelonInfo(typeof(InfinityFires.Implementation), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
